19ba38c7a8a4dc1536ea84aea6537d2e3269e6f6
baesuhan <105197524+uh004@users.noreply.github.com>
2024-05-03 11:21:14 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# save this as app.py
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+if __name__ == '__main__':
+  app.run(debug=True)
